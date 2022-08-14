6c183dc1c47d7383d11b3d12288237044bc588bb
Lukman <lukmansanni60@gmail.com>
2022-08-15 00:44:50 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.scss';
 import { Box, Btn } from 'components';
 import { ReactComponent as Idea } from 'assets/icons/idea.svg';
+import { todos } from 'data';
 
 const App = () => {
   return (
@@ -60,60 +61,11 @@ const App = () => {
       </aside>
 
       <main className="main">
-        <Box
-          type="main"
-          votes="112"
-          topic="Add tags for solutions"
-          details="Easier to search for solutions based on specific stack."
-          count="2"
-          btnName="Enhancement"
-        />
-
-        <Box
-          type="main"
-          votes="99"
-          topic="Add a dark theme option"
-          details="It would help people with light sensitivities and who prefer dark mode."
-          count="4"
-          btnName="Feature"
-        />
-
-        <Box
-          type="main"
-          votes="65"
-          topic="Q&A within the challenge hubs"
-          details="Challenge-specific Q&A would make for easy reference."
-          count="1"
-          btnName="Feature"
-        />
-
-        <Box
-          type="main"
-          votes="51"
-          topic="Allow image/video upload to feedback"
-          details="Images and screencasts can enhance comments on solutions."
-          count="2"
-          btnName="Enhancement"
-        />
-
-        <Box
-          type="main"
-          votes="42"
-          topic="Ability to follow others"
-          details="Stay updated on comments and solutions other people post."
-          count="3"
-          btnName="Feature"
-        />
-
-        <Box
-          type="main"
-          votes="3"
-          topic="Q&A within the challenge hubs"
-          details="Challenge preview images are missing when you apply a filter."
-          count="0"
-          btnName="Bug"
-        />
+        {todos.map((todo, idx) => (
+          <Box key={idx} type="main" {...todo} />
+        ))}
       </main>
+      
     </div>
   );
 };
---
+++ src/data/index.js
@@ -0,0 +1,45 @@
+export const todos = [
+  {
+    votes: 112,
+    topic: 'Add tags for solutions',
+    details: 'Easier to search for solutions based on specific stack.',
+    count: 2,
+    btnName: 'Enhancement',
+  },
+  {
+    votes: 99,
+    topic: 'Add a dark theme option',
+    details:
+      'It would help people with light sensitivities and who prefer dark mode.',
+    count: 4,
+    btnName: 'Feature',
+  },
+  {
+    votes: 65,
+    topic: 'Q&A within the challenge hubs',
+    details: 'Challenge-specific Q&A would make for easy reference.',
+    count: 1,
+    btnName: 'Feature',
+  },
+  {
+    votes: 51,
+    topic: 'Allow image/video upload to feedback',
+    details: 'Images and screencasts can enhance comments on solutions.',
+    count: 2,
+    btnName: 'Enhancement',
+  },
+  {
+    votes: 42,
+    topic: 'Ability to follow others',
+    details: 'Stay updated on comments and solutions other people post.',
+    count: 3,
+    btnName: 'Feature',
+  },
+  {
+    votes: 3,
+    topic: 'Q&A within the challenge hubs',
+    details: 'Challenge preview images are missing when you apply a filter.',
+    count: 0,
+    btnName: 'Bug',
+  },
+];
